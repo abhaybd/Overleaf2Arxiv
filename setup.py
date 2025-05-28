1b7265fc82619fa0e084b826154b8b781bfa749f
Abhay D <a.deshpande012@gmail.com>
2025-05-28 13:01:58 -0700

    Fix github release ci

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="overleaf2arxiv",
-    version="0.2.0",
+    version="0.2.1",
     packages=["overleaf2arxiv"],
     install_requires=[
         "pyoverleaf",
